7724deb65898efd06fd77f1faee6ec94baaf1f6f
tshewangdorji7257 <02230312.cst@rub.edu.bt>
2024-08-27 23:58:47 -0700

    first commit

---
+++ project.js
@@ -0,0 +1,34 @@
+// const showProject = () => {
+//     const buttons = document.querySelectorAll('.more-details');
+//     buttons.forEach((button) => {
+//         button.addEventListener('click', () => {
+//             const parentElement = button.parentElement;
+//             if (button.textContent === 'Show project') {
+//                 parentElement.classList.remove('close-project')
+//                 parentElement.classList.add('open-project')
+//                 parentElement.querySelector('iframe').style.opacity = "1";
+//                 button.textContent = 'Show less'
+//             } else {
+//                 parentElement.classList.remove('open-project')
+//                 parentElement.classList.add('close-project')
+//                 button.textContent = 'Show project'
+//                 parentElement.querySelector('iframe').style.opacity = "0";
+//             }
+//         })
+//     })
+// }
+
+// export default showProject;
+const showProject = () => {
+    const buttons = document.querySelectorAll('.more-details');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const githubLink = button.getAttribute('data-github-link');
+            if (githubLink) {
+                window.open(githubLink, '_blank');
+            }
+        });
+    });
+};
+
+export default showProject;
